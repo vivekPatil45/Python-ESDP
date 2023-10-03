8622fc06684978b5af623df03c653bf85f0c2ccc
vivekPatil45 <vivekrp4503@gmail.com>
2023-10-03 16:07:01 +0530

    garbage collection

---
+++ Garbage.py
@@ -0,0 +1,27 @@
+import gc
+import time
+import sys
+
+# Destructor __del__
+# class Time:
+#     def __init__(self) :
+#         print("Constructor is called")
+#     def __del__(self):
+#         print("Destructor is called")
+
+# t= Time()
+# t =None
+# print("Application is ended")
+
+
+
+# del used for destroying object
+# class Time:
+#     def __init__(self) :
+#         print("Constructor is called")
+#     def __del__(self):
+#         print("Destructor is called")
+
+# t= Time()
+# del t
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,19 @@
-for i in range(len(l)-1,-1,-1):
-    ans+=(l[i]+" ")
+class Employee:
+#     def __init__(self,no,name,sal) :
+#         self.eno = no
+#         self.ename = name
+#         self.esal = sal
 
-print(ans)
+#     def showDetails(self):
+#         print("Employee no       : ",self.eno)
+#         print("Employee ename    : ",self.ename)
+#         print("Employee salary   : ",self.esal)
+
+# class Test:
+#     def updates(Employee):
+#         Employee.esal = Employee.esal+8000
+#         Employee.showDetails()
+
+# e = Employee(101,"Vivek",20000)
+# e.showDetails()
+# Test.updates(e)
