7e71ade06f6502e04a449524ce5c536ea7943e08
vivekPatil45 <vivekrp4503@gmail.com>
2023-10-04 15:54:50 +0530

    inheritance question

--- Inheritanceq.py
+++ Inheritanceq.py
@@ -51,4 +51,82 @@
 # s3.study()
 
 
+# Q3
+
+# class Shape:
+#     def __init__(self):
+#         pass
+#     def area(self):
+#         self.ar = 0
+#     def perimeter(self):
+#         self.per = 0
+    
+# class Circle(Shape):
+#     def __init__(self,rad):
+#         super().__init__()
+#         self.rad = rad
+    
+#     def area(self):
+#         super().area()
+#         self.ar = 3.14 * self.rad * self.rad
+#         print("Area Of Circle = ",self.ar)
+
+#     def perimeter(self):
+#         super().perimeter()
+#         self.per = 2 * 3.14 * self.rad
+#         print("Perimeter Of Circle = ",self.per)
+
+    
+# class Rectangle(Shape):
+#     def __init__(self,l,b):
+#         super().__init__()
+#         self.l = l
+#         self.b = b
+
+    
+#     def area(self):
+#         super().area()
+#         self.ar = self.l * self.b
+#         print("Area Of Circle = ",self.ar)
+
+#     def perimeter(self):
+#         super().perimeter()
+#         self.per = 2*self.l + 2*self.b
+#         print("Perimeter Of Circle = ",self.per)
+
+
+# Q4
+
+class Product:
+    def __init__(self,name,price,qty):
+        self.name = name
+        self.price = price
+        self.qty = qty
+
+class Book(Product):
+    def __init__(self, name, price, qty,author,gener):
+        super().__init__(name, price, qty)
+        self.author = author
+        self.gener = gener
+
+    def display_details(self):
+        print("Book Name ",self.name)
+        print("Book Price ",self.name)
+        print("Product Quantity ",self.qty)
+        print("Author Name ",self.name)
+        print("Book Genere ",self.gener)
+
+
+class Electronics(Product):
+    def __init__(self, name, price, qty,author,gener):
+        super().__init__(name, price, qty)
+        self.author = author
+        self.gener = gener
+
+    def display_details(self):
+        print("Book Name ",self.name)
+        print("Book Price ",self.name)
+        print("Product Quantity ",self.qty)
+        print("Author Name ",self.name)
+        print("Book Genere ",self.gener)
 
