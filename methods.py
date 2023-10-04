3ef35c0002d1de4d8b0e1443ac3f00fe07baaed7
vivekPatil45 <vivekrp4503@gmail.com>
2023-10-04 16:45:11 +0530

    polymorphism

---
+++ methods.py
@@ -0,0 +1,50 @@
+# Method Overrriding
+# class Parent :
+#     def prop(self):
+#         print("Cash, Gold , ")
+    
+#     def bike(self):
+#         print("Splender")
+
+# class Child(Parent):
+#     def bike(self):
+#         print("Avenger")
+
+
+# c= Child()
+# c.prop()
+# c.bike()
+
+
+# Constructor Overridding
+# class Parent:
+#     def __init__(self) :
+#         print("Parent Constructor")
+
+# class Child(Parent):
+#     def __init__(self) :
+#         print("Child Constructor")
+# c = Child()
+    
+
+
+
+
+
+
+# 
+# class A:
+#     def show(self):
+#         print("Class A")
+
+# class B:
+#     def show(self):
+#         print("Class B")
+
+# class C(A,B):
+#     def showC(self):
+#         print("Class C")
+
+# obj = C()
+# obj.show()
+# obj.showC()
